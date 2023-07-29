6b34ee0d34d1af5bf4ca7e3498458f7f7b16bb73
edubew <edubew98@gmail.com>
2023-07-29 09:07:47 +0300

    Update the necessary routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+# greetings = [
+#     "Good morning🤗!",
+#     "Bonjour😊!",
+#     "Buenos dias😊!",
+#     "Habari za asubuhi😊!",
+#     "Günaydın😊!"
+#   ]
+  
+  
+#   greetings.each do |message|
+#     Greeting.create(message: message)
+# end
+
 Greeting.create(message: 'Good morning🤗!')
 Greeting.create(message: 'Bonjour😊!')
 Greeting.create(message: 'Buenos dias😊!')

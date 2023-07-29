dbec510834b9fa1e36377ffc080700854f88a0b0
edubew <edubew98@gmail.com>
2023-07-29 09:20:02 +0300

    Fix rubocop errors

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,7 +1,6 @@
 class GreetingsController < ApplicationController
-    def random_greeting
-      greeting = Greeting.order('RANDOM()').first
-      render json: { greeting: greeting.message }
-    end
+  def random_greeting
+    greeting = Greeting.order('RANDOM()').first
+    render json: { greeting: greeting.message }
+  end
 end
-  
